40a35495b2c18446b770320f1dfaaa195684bbe7
sokkanji <sskkanji@gmail.com>
2020-10-04 22:08:54 +0900

    Add: database sql file

---
+++ test.sql
@@ -0,0 +1,17 @@
+create table signup(
+id int(10) not null auto_increment,
+userid varchar(30),
+userpw varchar(30),
+displayName varchar(30),
+email varchar(30),
+primary key(id)
+);
+
+create table borad(
+id int(10) not null auto_increment,
+writer varchar(50),
+title varchar(30),
+content varchar(300),
+regdate  timestamp default current_timestamp on update current_timestamp,
+primary key(id)
+);
